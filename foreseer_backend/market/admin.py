ef0a683557d88eaf554b6946aa28c32cf54e425e
adityachakra16 <adityachakra16@gmail.com>
2024-08-09 17:09:37 +0530

    add admin pages for models

--- foreseer_backend/market/admin.py
+++ foreseer_backend/market/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from market import models
+
+
+# Register your models here.
+@admin.register(models.Market)
+class MarketAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
--- foreseer_backend/user/admin.py
+++ foreseer_backend/user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from user import models
+
+
+# Register your models here.
+@admin.register(models.UserDetails)
+class UserDetailsAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+    )
